34a8be798d6d0c620b943fc8a2aa57968de5efe7
VladTrokun <trokunvlad@gmail.com>
2025-09-24 12:59:25 +0300

    Add files via upload

---
+++ script.py
@@ -0,0 +1,49 @@
+years = 17
+#створити змінну years зі значенням 17
+print("years", type(years), ":", years)
+#Виводимр змінну,тип змінної, значення змінної
+
+price = 10.99
+# створюємо змінну price зі значенням 10.99
+print("price", type(price), ":", price)
+# Виводимо назву змінної, її тип та значення
+
+city = "Луцьк"
+# створюємо змінну city зі значенням "Луцьк"
+print("city", type(city), ":", city)
+# Виводимо назву змінної, її тип та значення
+
+is_student = True
+# створюємо змінну is_student зі значенням True
+print("is_student", type(is_student), ":", is_student)
+# Виводимо назву змінної, її тип та значення
+
+numbers = [1, 2, 3, 4, 5]
+# створюємо список numbers
+print("numbers", type(numbers), ":", numbers)
+# Виводимо назву змінної, її тип та значення
+
+colors = ("зелений", "сірий", "блакитний")
+# створюємо множину colors
+print("colors", type(colors), ":", colors)
+# Виводимо назву змінної, її тип та значення
+
+birth_date = (2008, 6, 30)
+# створюємо кортеж birth_date
+print("birth_date,",type(birth_date),":",birth_date)
+# Виводимо назву змінної, її тип та значення
+
+student = {"name": "Влад", "age": 17}
+# створюємо словник student
+print("student", type(student), ":", student)
+# Виводимо назву змінної, її тип та значення
+
+a=25
+b=5
+print("a+b=",a+b) #додавання
+print("a-b=",a-b) #віднімання
+print("a*b=",a*b) #множення
+print("a/b=",a/b) #ділення
+print("a%b=",a%b) #остача
+print("a**b=",a**b) #степінь
+print("a//b=",a//b) #ділення без остачі

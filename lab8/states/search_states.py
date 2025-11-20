896fbc8748f8b8fc087c029e5822e84ef998bfe5
VladTrokun <trokunvlad@gmail.com>
2025-11-20 10:42:33 +0200

    Delete lab8 directory

--- lab8/states/search_states.py
+++
@@ -1,6 +0,0 @@
-from aiogram.fsm.state import StatesGroup, State
-
-class SearchState(StatesGroup):
-    waiting_for_title = State()
-    waiting_for_genre = State()
-    waiting_for_keyword = State()

46fff5f83a1afecde1180b6b5c51e0a78a70e368
VladTrokun <trokunvlad@gmail.com>
2025-12-04 12:17:42 +0200

    Delete lab8 directory

--- lab8/states/search_states.py
+++
@@ -1,8 +0,0 @@
-from telebot.handler_backends import StatesGroup, State # Імпорт класів станів для telebot
-
-# Клас для керування станами користувача (FSM)
-class SearchState(StatesGroup):
-    # Стан очікування введення назви
-    waiting_for_title = State()
-    # Стан очікування введення жанру
-    waiting_for_genre = State()
